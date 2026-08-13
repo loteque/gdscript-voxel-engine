ba33ded5c231efaca318df00d803885d29713b6d
loteque <69282314+loteque@users.noreply.github.com>
2026-08-13 09:42:10 -0700

    Name runtime workload experiment

--- demo/experiments/RuntimeExperimentShell.gd
+++ demo/experiments/RuntimeExperimentShell.gd
@@ -1,6 +1,10 @@
 extends Node
 
-const TITLE := "Chunk Streamer Runtime Workload"
+const TITLE := "Chunk Streamer Runtime Workload Isolation"
+const REPETITIONS := 3
 
 func get_title() -> String:
 	return TITLE
+
+func get_expected_run_count() -> int:
+	return 12
