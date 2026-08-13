e1fc4dca930f009a4f8f30d4721e7a712941399f
loteque <69282314+loteque@users.noreply.github.com>
2026-08-13 09:40:15 -0700

    Add diagnostic residency state

--- demo/experiments/RuntimeWorkloadIsolationChunkStreamer.gd
+++ demo/experiments/RuntimeWorkloadIsolationChunkStreamer.gd
@@ -11,9 +11,12 @@ enum WorkloadMode {
 }
 
 var workload_mode: WorkloadMode = WorkloadMode.NORMAL_RUNTIME
+var _diagnostic_resident: Dictionary[Vector3i, bool] = {}
 
 func set_workload_mode(mode: WorkloadMode) -> void:
+	clear_chunks()
 	workload_mode = mode
+	_diagnostic_resident.clear()
 
 func get_workload_mode_name() -> String:
 	match workload_mode:
@@ -22,6 +25,24 @@ func get_workload_mode_name() -> String:
 		WorkloadMode.NO_SCENE_INTEGRATION: return "no_scene_integration"
 		WorkloadMode.MINIMAL_RESIDENCY: return "minimal_residency"
 	return "unknown"
+
+func is_chunk_loaded(coordinate: Vector3i) -> bool:
+	if workload_mode <= WorkloadMode.HIDDEN_GEOMETRY:
+		return super(coordinate)
+	return _diagnostic_resident.has(coordinate)
+
+func get_loaded_coordinates() -> Array[Vector3i]:
+	if workload_mode <= WorkloadMode.HIDDEN_GEOMETRY:
+		return super()
+	var coordinates: Array[Vector3i] = []
+	coordinates.assign(_diagnostic_resident.keys())
+	coordinates.sort_custom(_coordinate_less_than)
+	return coordinates
+
+func get_chunk_instance(coordinate: Vector3i) -> MeshInstance3D:
+	if workload_mode <= WorkloadMode.HIDDEN_GEOMETRY:
+		return super(coordinate)
+	return null
 
 func _complete_load_request(request) -> void:
 	super(request)
